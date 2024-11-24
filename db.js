4d93863fc59052e9a72fa04af710973725fc69db
kkronggiscool <crongie999@gmail.com>
2024-11-23 21:55:58 -0500

    skibidi add fix

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql2');
-require('dotenv').config(); // Load .env variables
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
 
-// Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'pw123',
+  database: process.env.DB_NAME || 'tubeu',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-module.exports = pool.promise();
+module.exports = pool;
