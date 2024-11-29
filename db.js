b5a740d86ab2948580a38aecf0d0830a319b8eb2
kkronggiscool <crongie129@gmail.com>
2024-11-29 14:29:01 -0500

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,19 @@ const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 dotenv.config();
 
+/*
+  hi there, hackers!
+  you hacked my private repo, somehow, i'm not proud of you, as a matter of fact, kys for doing that.
+  great job on hacking me, here's some of my database shit you can have.
+  fuck you --kkronggiscool
+  oh, wait, this database was deleted a while ago. still, fuck you!
+*/
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 99.96.25.63
+  user: 'root'
+  password: 'xjclkjx'
+  database: 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
